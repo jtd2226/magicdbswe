4f4eddba2785085650cfbee72ce836600ce88a74
jtd2226 <joeldavi@utexas.edu>
2017-04-20 15:36:38 -0500

    visualization at 90%

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ import config
 import models
 import random
 import json
+import urllib.request
 
 from config import POSTS_PER_PAGE
 from models import db, app, MSubtype, MCard, MArtist, MSet, Resource, api
@@ -10,6 +11,66 @@ from flask import Flask, render_template, request, Markup, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_paginate import Pagination # make sure to pyhton3 -m pip install
 from collections import OrderedDict
+
+#--------------
+# VISUALIZATION
+#--------------
+
+purl = "http://ggnoswe.me/api/platforms/"
+surl = "http://ggnoswe.me/api/studios/"
+gurl = "http://ggnoswe.me/api/games/"
+
+platforms = [
+  {
+    "id": 41, 
+    "name": "Wii U"
+  },    
+  {
+    "id": 37, 
+    "name": "Nintendo 3DS"
+  }, 
+  {
+    "id": 48, 
+    "name": "PlayStation 4"
+  }, 
+  {
+    "id": 49, 
+    "name": "Xbox One"
+  }, 
+  {
+    "id": 130, 
+    "name": "Nintendo Switch"
+  },
+  {
+    "id": 12, 
+    "name": "Xbox 360"
+  }, 
+  {
+    "id": 9, 
+    "name": "PlayStation 3"
+  }
+]
+
+root = {"name": "flare",
+        "description": "flare",
+        "children": []}
+
+for p in platforms:
+    cp = json.loads(urllib.request.urlopen(purl + str(p["id"])).read().decode())
+    d = {}
+    d["name"] = p["name"]
+    d["description"] = cp["summary"]
+    d["children"] = []
+    count = 0;
+    for s in cp["studios"]:
+        cs = json.loads(urllib.request.urlopen(surl + str(s)).read().decode())
+        d["children"].append({"name": cs["name"],"description": cs["description"], "size": len(cs["games"])})
+        count = count + 1 
+
+    root["children"].append(d)
+
+with open('templates/flare.json', 'w') as outfile:
+    json.dump(root, outfile)
 
 #-------
 #  API
--- app/populateTest.py
+++ app/populateTest.py
@@ -1,104 +1,59 @@
 import models
 import json
+import urllib.request
 
-#from mtgsdk import Card, Set, Subtype
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from models import db, MCard, MSet, MArtist, MSubtype
+purl = "http://ggnoswe.me/api/platforms/"
+surl = "http://ggnoswe.me/api/studios/"
+gurl = "http://ggnoswe.me/api/games/"
 
-# ----------
-#  SUBTYPES
-# ----------
+platforms = [
+  {
+    "id": 41, 
+    "name": "Wii U"
+  },    
+  {
+    "id": 37, 
+    "name": "Nintendo 3DS"
+  }, 
+  {
+    "id": 48, 
+    "name": "PlayStation 4"
+  }, 
+  {
+    "id": 49, 
+    "name": "Xbox One"
+  }, 
+  {
+    "id": 130, 
+    "name": "Nintendo Switch"
+  },
+  {
+    "id": 12, 
+    "name": "Xbox 360"
+  }, 
+  {
+    "id": 9, 
+    "name": "PlayStation 3"
+  }
+]
 
-# db.session.commit()
-# db.drop_all()
-# db.create_all()
-subCache = dict()
-artCache = dict()
-setCache = dict()
+root = {"name": "flare",
+ 		"description": "flare",
+ 		"children": []}
 
+for p in platforms:
+	cp = json.loads(urllib.request.urlopen(purl + str(p["id"])).read().decode())
+	d = {}
+	d["name"] = p["name"]
+	d["description"] = cp["summary"]
+	d["children"] = []
+	count = 0;
+	for s in cp["studios"]:
+		cs = json.loads(urllib.request.urlopen(surl + str(s)).read().decode())
+		d["children"].append({"name": cs["name"],"description": cs["description"], "size": len(cs["games"])})
+		count = count + 1 
 
-for stype in db.session.query(MSubtype).all():
-	print(len(db.session.query(MSubtype).filter_by(name=stype.name).first().ssets.all()))
+	root["children"].append(d)
 
-# with open('stypeCache.json') as scache:
-#     subtypes = json.load(scache)["subtypes"]
-
-# subCache["none"] = MSubtype("none", 0)
-
-# for stype in subtypes:
-# 	temp = MSubtype(stype["name"], stype["numCards"])
-# 	subCache[stype["name"]] = temp
-# 	#db.session.add(temp)
-# 	#db.session.commit()
-
-# # ------
-# # SETS
-# # ------
-# print("starting sets")
-# with open('setCache.json') as sc:
-#     sets = json.load(sc)["allsets"]
-
-# for iset in sets:
-
-# 	set_subTypes = list()
-# 	for stype in iset["subTypes"]:
-# 		set_subTypes.append(subCache[stype])
-
-# 	if len(set_subTypes) == 0:
-# 		set_subTypes = [subCache["none"]]
-
-# 	temp = MSet(iset["code"], iset["name"], iset["rDate"], iset["block"], iset["numCards"], iset["symbol"], set_subTypes)
-# 	setCache[iset["code"]] = temp
-
-# 	#db.session.add(temp)
-# 	#db.session.commit()
-
-# # ---------
-# # ARTISTS
-# # ---------
-# print("starting artists")
-# with open('artistCache.json') as ac:
-#     artists = json.load(ac)
-
-# for artist in artists:
-
-# 	artists_sets = list()
-# 	for a in artists[artist]["sets"]:
-# 		artists_sets.append(setCache[a])
-
-# 	if len(artists_sets) == 0:
-# 		artists_sets = [setCache["none"]]
-
-# 	temp = MArtist(artist, artists[artist]["numCards"], artists[artist]["numSets"], artists_sets)
-# 	artCache[artist] = temp
-
-# 	#db.session.add(temp)
-# 	#db.session.commit()
-
-
-# # -------
-# # CARDS
-# # -------
-# print ("starting cards")
-# with open('cardCache.json') as cc:
-#     cards = json.load(cc)["allcards"]
-
-# cardCount = 0
-# for card in cards:
-# 	cardCount = cardCount + 1
-# 	print(str(cardCount) + " card(s) added")
-# 	card_subTypes = list()
-# 	for stype in card["subtype"]:
-# 		card_subTypes.append(subCache[stype])
-
-# 	if len(card_subTypes) == 0:
-# 		card_subTypes = [subCache["none"]]
-
-# 	temp = MCard(card["cardID"], card["name"], card["mainType"], card_subTypes, card["text"], card["expansionSet"], card["manaCost"], ", ".join(card["color"]), card["power"], card["toughness"], card["art"], card["rarity"], card["artist"])
-	
-# 	if db.session.query(MCard).filter_by(cardId=card["cardID"]).first() is None:
-# 		db.session.add(temp)
-# 		db.session.commit()
-
-
+with open('templates/flare.json', 'w') as outfile:
+    json.dump(root, outfile)
